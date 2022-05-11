e6d7075a1aa4af2a0412f19c0efc2f031bf094e9
amagramer <amagramer@gmail.com>
2022-05-11 17:06:44 +0900

    datasource connection pool test

--- src/test/java/com/example/springdbexam/conection/ConnectionTest.java
+++ src/test/java/com/example/springdbexam/conection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package com.example.springdbexam.conection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -29,6 +30,18 @@ public class ConnectionTest {
         //그러나 spring에서 DataSource를 구현한 DriverManagerDataSource를 제공한다.
         DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
         useDataSource(dataSource);
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException { //커넥션 풀링: HikariProxyConnection(Proxy) -> JdbcConnection(Target)
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+        useDataSource(dataSource);
+        Thread.sleep(1000); //커넥션 풀에서 커넥션 생성 시간 대기
     }
 
     private void useDataSource(DataSource dataSource) throws SQLException {
