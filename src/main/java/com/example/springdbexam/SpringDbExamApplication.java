fbabbbbf8e7c84eca78398d60f55df50bcbe3d6d
amagramer <amagramer@gmail.com>
2022-05-11 14:33:24 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/springdbexam/SpringDbExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdbexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDbExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDbExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springdbexam/SpringDbExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdbexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDbExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
